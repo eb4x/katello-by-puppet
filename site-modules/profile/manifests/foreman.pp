b6a1b06a8fd4fc21449266b670c6acdb9b6af4bd
Erik Berg <erikberg@usit.uio.no>
2023-10-02 19:05:43 +0200

    profile: foreman (install)

--- site-modules/profile/manifests/foreman.pp
+++ site-modules/profile/manifests/foreman.pp
@@ -1,8 +1,22 @@
 class profile::foreman (
+  String $initial_admin_password = 'changeme',
+  String $db_password = 'changeme',
 ) {
 
   class { '::foreman::repo':
     repo => '3.7',
+  }
+
+  class { '::puppet':
+    server                => true,
+    server_external_nodes => '',
+  }
+
+  class { '::foreman':
+    version => 'latest',
+    plugin_version => 'latest',
+    initial_admin_password => $initial_admin_password,
+    db_password => $db_password,
   }
 
 }
