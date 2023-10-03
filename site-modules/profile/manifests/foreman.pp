e24b99114a92322a700c101618215575d0b5ea36
Erik Berg <erikberg@usit.uio.no>
2023-10-03 15:03:39 +0200

    Fix order/dependency in foreman

--- site-modules/profile/manifests/foreman.pp
+++ site-modules/profile/manifests/foreman.pp
@@ -4,6 +4,9 @@ class profile::foreman (
   include ::foreman
   include ::foreman::repo
 
+  Class['foreman::repo']
+  -> Class['foreman::install']
+
   class { '::puppet':
     server                => true,
     server_external_nodes => '',
