934ced845df1ed0b742791b041990b287b517b4e
Erik Berg <erikberg@usit.uio.no>
2023-10-03 12:51:35 +0200

    profile: katello (dependencies/order)

--- site-modules/profile/manifests/katello.pp
+++ site-modules/profile/manifests/katello.pp
@@ -4,4 +4,13 @@ class profile::katello (
   include ::katello
   include ::katello::repo
 
+  Class['katello::repo']
+  -> Class['certs::install', 'katello']
+
+  include ::pulpcore::repo
+  Class['pulpcore::repo']
+  -> Package['postgresql-evr']
+
+  include ::candlepin::repo
+
 }
